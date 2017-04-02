6118cf80c414510100a235c78fd482e8ccace387
Matt Kenney <mkenney@mjk-tech.com>
2017-04-02 14:42:43 +0000

    add 'trust proxy' - it is, and we want the session cookie to be TLS-only

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.set('strict routing', true);
+app.set('trust proxy', 1);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
